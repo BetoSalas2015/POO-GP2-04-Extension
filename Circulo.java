27b9be699b7fc2bc18d192e29721107737bc8507
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-02-25 11:03:45 -0600

    Sesión 17 - 23/02/2022

---
+++ Circulo.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Circulo here.
+ * 
+ * @author Roberto Salazar Marquez
+ * @version 1.0
+ */
+public class Circulo extends Punto
+{
+    private int radio;
+    
+    public Circulo(int x, int y, int r) {
+        super(x, y);
+        setRadio(r);
+    }
+    
+    public void setRadio(int r) {
+        radio = (r >= 0 ) ? r : 1;
+    }
+    
+    public int getRadio() {
+        return radio;
+    }
+    
+    public String toString() {
+        return "Centro " + super.toString() + ", Radio = " + radio; 
+    }
+}
+
+
+
+
+
+
